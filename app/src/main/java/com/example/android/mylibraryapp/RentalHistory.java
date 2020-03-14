18bdff01d3da9b81ceec93e81c443168536dd431
Tay <taylordaniels21@gmail.com>
2020-03-13 22:14:33 -0700

    Rental Boundary created

---
+++ app/src/main/java/com/example/android/mylibraryapp/RentalHistory.java
@@ -0,0 +1,14 @@
+package com.example.android.mylibraryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentalHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rental_history);
+    }
+}
