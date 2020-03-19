32eaee2ca0b3cbea6d0735ea87a8b7f8c606999d
spidervoelker <spidervoelker@gmail.com>
2020-03-18 19:24:52 -0700

    Layout for edit book/edit qty activity done; will add style options later

---
+++ app/src/main/java/com/example/android/mylibraryapp/ControlObjects/EditBookDetailsActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.mylibraryapp.ControlObjects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.mylibraryapp.R;
+
+public class EditBookDetailsActivity extends AppCompatActivity {
+
+    // TODO: Set up fields to grab appropriate data from db onStart
+    // TODO: Set up methods to overwrite what is in database
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_book_details);
+
+
+    }
+
+}
