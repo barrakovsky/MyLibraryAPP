e5451ca4d39a12f2465aa84719181dacb544455a
kleang <44731051+kleang@users.noreply.github.com>
2020-03-30 16:11:57 -0700

    Updated Book Entity

--- app/src/main/java/com/example/android/mylibraryapp/EntityObjects/Book.java
+++ app/src/main/java/com/example/android/mylibraryapp/EntityObjects/Book.java
@@ -3,117 +3,73 @@ package com.example.android.mylibraryapp.EntityObjects;
 import java.time.Year;
 
 public class Book {
-    private String ISBN;
-    private String title;
     private String author;
-    private Year publishingYear;
-    private String genre;
-    private int numPages;
-    private String publisher;
-    private String summary;
-    private int totQty;
     private int availableQty;
+    private String genre;
+    private long isbn;
+    private int numberOfPages;
+    private String publisher;
+    private int publishingYear;
+    private String summary;
+    private String title;
+    private int totQty;
 
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public void setISBN(String bookISBN) {
-        ISBN = bookISBN;
+    public long getIsbn() {
+        return isbn;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String bookTitle) {
-        title = bookTitle;
-    }
-
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String bookAuthor) {
-        author = bookAuthor;
-    }
-
-    public Year getPublishingYear() {
+    public int getPublishingYear() {
         return publishingYear;
-    }
-
-    public void setPublishingYear(Year bookPublishingYear) {
-        publishingYear = bookPublishingYear;
     }
 
     public String getGenre() {
         return genre;
     }
 
-    public void setGenre(String bookGenre) {
-        genre = bookGenre;
-    }
-
-    public int getNumPages() {
-        return numPages;
-    }
-
-    public void setNumPages(int bookNumPages) {
-        numPages = bookNumPages;
+    public int getNumberOfPages() {
+        return numberOfPages;
     }
 
     public String getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String bookPublisher) {
-        publisher = bookPublisher;
-    }
-
     public String getSummary() {
         return summary;
-    }
-
-    public void setSummary(String bookSummary) {
-        summary = bookSummary;
     }
 
     public int getTotQty() {
         return totQty;
     }
 
-    public void setTotQty(int bookTotQty) {
-        totQty = bookTotQty;
-    }
-
     public int getAvailableQty() {
         return availableQty;
     }
 
-    public void setAvailableQty(int bookAvailableQty) {
-        availableQty = bookAvailableQty;
+    public Book() {
+        //empty constructor
     }
 
-    public Book(String ISBN, String title, String author, Year publishingYear, String genre, int numPages, String publisher, String summary) {
-        this.ISBN = ISBN;
+    public Book(long isbn, String title, String author, int publishingYear, String genre, int numberOfPages, String publisher, String summary, int totQty, int availableQty) {
+        this.isbn = isbn;
         this.title = title;
         this.author = author;
         this.publishingYear = publishingYear;
         this.genre = genre;
-        this.numPages = numPages;
+        this.numberOfPages = numberOfPages;
         this.publisher = publisher;
         this.summary = summary;
+        this.totQty = totQty;
+        this.availableQty = availableQty;
     }
 
-    public Book(String ISBN, String title, String author, Year publishingYear, int numPages, String publisher) {
-        this.ISBN = ISBN;
-        this.title = title;
-        this.author = author;
-        this.publishingYear = publishingYear;
-        this.genre = "N/A";
-        this.numPages = numPages;
-        this.publisher = publisher;
-        this.summary = "N/A";
-    }
 
 }
