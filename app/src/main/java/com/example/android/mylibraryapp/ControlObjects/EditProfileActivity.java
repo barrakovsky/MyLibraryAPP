1e71e1ef713e1ad8b2eaf3627729d8f8cf82ed99
Tay <taylordaniels21@gmail.com>
2020-03-30 18:09:41 -0700

    Completed Edit Profile Functionality

--- app/src/main/java/com/example/android/mylibraryapp/ControlObjects/EditProfileActivity.java
+++ app/src/main/java/com/example/android/mylibraryapp/ControlObjects/EditProfileActivity.java
@@ -1,16 +1,97 @@
 package com.example.android.mylibraryapp.ControlObjects;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.android.mylibraryapp.EntityObjects.User;
 import com.example.android.mylibraryapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EditProfileActivity extends BaseActivity {
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    String uid;
+    String userID = Long.toString(System.currentTimeMillis());
+
+
+    EditText firstName_editText;
+    EditText lastName_editText;
+    EditText userName_editText;
+    EditText phoneNumber_editText;
+    EditText email_editText;
+    EditText password_editText;
+    Button saveButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        //Casting Views
+        firstName_editText = findViewById(R.id.editTextFirstName);
+        lastName_editText = findViewById(R.id.editTextLastName);
+        userName_editText = findViewById(R.id.editTextUserName);
+        phoneNumber_editText = findViewById(R.id.editTextPhone);
+        email_editText = findViewById(R.id.editTextEmail);
+        password_editText = findViewById(R.id.editTextPassword);
+        saveButton = findViewById(R.id.saveDataButton);
+
+
+
+        //Writing to the database
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+
+
+        uid = firebaseAuth.getCurrentUser().getUid();
+        final DocumentReference documentReference = firebaseFirestore.collection("Users").document(uid);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String firstName = firstName_editText.getText().toString();
+                final String lastName = lastName_editText.getText().toString();
+                final String userName = userName_editText.getText().toString();
+                final String phone = phoneNumber_editText.getText().toString();
+                final String email = email_editText.getText().toString();
+
+                final User information = new User(userID, firstName, lastName, userName, email, phone, false);
+
+
+                documentReference.update(
+                        "firstName", information.getfName(),
+                        "lastName", information.getlName(),
+                        "userName", information.getUserName(),
+                        "phone", information.getPhone(),
+                        "email", information.getEmail()
+                ).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(EditProfileActivity.this, "Information Updated", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        });
     }
 }
