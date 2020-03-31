bb3bb6e3c33447a02d82c31db82ed7b80652d7e4
kleang <44731051+kleang@users.noreply.github.com>
2020-03-31 10:35:00 -0700

    Update Request.java

--- app/src/main/java/com/example/android/mylibraryapp/EntityObjects/Request.java
+++ app/src/main/java/com/example/android/mylibraryapp/EntityObjects/Request.java
@@ -4,12 +4,69 @@ import java.time.Year;
 
 public class Request {
 
-    private String ISBN;
+    private long ISBN;
     private String title;
     private String author;
-    private Year publishingYear;
+    private int publishingYear;
     private String genre;
     private String publisher;
 
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+
+    public Request(long ISBN, String title, String author, int publishingYear, String genre, String publisher) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.publishingYear = publishingYear;
+        this.genre = genre;
+        this.publisher = publisher;
+    }
 
 }
