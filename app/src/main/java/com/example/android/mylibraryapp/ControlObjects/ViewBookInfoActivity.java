3fc5d26cad43785ba64a3adf2f8726f24c6d0362
kleang <44731051+kleang@users.noreply.github.com>
2020-04-05 18:59:29 -0700

    Add and remove favorite from book info boundary

--- app/src/main/java/com/example/android/mylibraryapp/ControlObjects/ViewBookInfoActivity.java
+++ app/src/main/java/com/example/android/mylibraryapp/ControlObjects/ViewBookInfoActivity.java
@@ -3,8 +3,11 @@ package com.example.android.mylibraryapp.ControlObjects;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,15 +17,28 @@ import com.example.android.mylibraryapp.Misc.ReviewAdapter;
 import com.example.android.mylibraryapp.R;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewBookInfoActivity extends BaseActivity {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference reviewRef;
+    private FirebaseUser user;
 
     private ReviewAdapter adapter;
 
@@ -37,6 +53,9 @@ public class ViewBookInfoActivity extends BaseActivity {
     private TextView publishing;
     private TextView pages;
     private TextView quantity;
+    private CheckBox favorite;
+
+    private ArrayList<DocumentReference> favorites = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +70,15 @@ public class ViewBookInfoActivity extends BaseActivity {
         publishing = findViewById(R.id.bookInfoPublishing);
         pages = findViewById(R.id.bookInfoPages);
         quantity = findViewById(R.id.bookInfoQty);
+        favorite = findViewById(R.id.bookInfoFavorite);
 
         book = (Book)getIntent().getSerializableExtra("Book");
         bookID = getIntent().getStringExtra("bookID");
 
         reviewRef = db.collection("Book").document(bookID).collection("Reviews");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        final CollectionReference faveRef = db.collection("Users").document(user.getUid()).collection("Favorites");
+        final DocumentReference bookRef = db.collection("Book").document(bookID);
 
         //set book information
         title.setText(book.getTitle());
@@ -85,6 +108,50 @@ public class ViewBookInfoActivity extends BaseActivity {
         else
             pages.setText(book.getNumberOfPages() + " pages");
         quantity.setText(book.getAvailableQty() + "/" + book.getTotQty() + " copies available");
+
+        //change status of favorite button based on favorites list
+        faveRef.whereEqualTo("bookRef", bookRef)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot documentSnapshot : task.getResult()) {
+                                if (documentSnapshot.get("bookRef").equals(bookRef))
+                                    favorite.setChecked(true);
+                                else favorite.setChecked(false);
+                            }
+                        }
+                    }
+                });
+
+        //add and remove from favorites
+        favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (favorite.isChecked()) {
+                    Map<String, Object> faveData = new HashMap<>();
+                    faveData.put("bookRef", bookRef);
+                    faveRef.document()
+                            .set(faveData);
+                    String message = "Added to favorites";
+                    Toast.makeText(ViewBookInfoActivity.this, message, Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Query query = faveRef.whereEqualTo("bookRef", bookRef);
+                    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot documentSnapshot : task.getResult())
+                                    faveRef.document(documentSnapshot.getId()).delete();
+                                Toast.makeText(ViewBookInfoActivity.this, "Removed from favorites", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
         // Setting up reviews at bottom of page
         TextView reviewHead = findViewById(R.id.book_info_rev_head_tx);
