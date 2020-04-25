fb79f32f4a709b4884703e7656aa5bab8e1ff2c1
kleang <44731051+kleang@users.noreply.github.com>
2020-04-25 15:39:01 -0700

    Update SearchResultsActivity.java

--- app/src/main/java/com/example/android/mylibraryapp/ControlObjects/SearchResultsActivity.java
+++ app/src/main/java/com/example/android/mylibraryapp/ControlObjects/SearchResultsActivity.java
@@ -3,18 +3,39 @@ package com.example.android.mylibraryapp.ControlObjects;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android.mylibraryapp.EntityObjects.Book;
 import com.example.android.mylibraryapp.Misc.BookAdapter;
 import com.example.android.mylibraryapp.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class SearchResultsActivity extends BaseActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -22,6 +43,7 @@ public class SearchResultsActivity extends BaseActivity {
 
     private BookAdapter adapter;
     private RecyclerView recyclerView;
+    private SearchView searchView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,7 +51,21 @@ public class SearchResultsActivity extends BaseActivity {
         Intent i = getIntent();
         super.isAdmin = i.getBooleanExtra("isAdmin", false);
         setContentView(R.layout.activity_search_result);
+        searchView = findViewById(R.id.search_view);
         setUpRecyclerView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                search(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
     private void setUpRecyclerView() {
@@ -56,6 +92,19 @@ public class SearchResultsActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void search(String search) {
+        Query query = bookRef.orderBy("title", Query.Direction.ASCENDING).startAt(search).endAt(search + "\uf8ff");
+
+        if (search.isEmpty())
+            query = bookRef.orderBy("title", Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<Book> option = new FirestoreRecyclerOptions.Builder<Book>()
+                .setQuery(query, Book.class)
+                .build();
+
+        adapter.updateOptions(option);
     }
 
     @Override
